1889f1dfa98e2b41901bbf2d2d80923d590225eb
amoi9 <aileenzhang7+github@gmail.com>
2019-02-18 13:06:28 -0800

    improves

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,8 +3,8 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
+/*
+ * Please note that the Eigen library does not initialize
  *   VectorXd or MatrixXd objects with zeros upon creation.
  */
 
@@ -14,27 +14,27 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
-  x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+    /**
+     * TODO: predict the state
+     */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+    /**
+     * TODO: update the state by using Kalman Filter equations
+     */
     VectorXd z_pred = H_ * x_;
     VectorXd y = z - z_pred;
     MatrixXd Ht = H_.transpose();
@@ -42,7 +42,7 @@ void KalmanFilter::Update(const VectorXd &z) {
     MatrixXd Si = S.inverse();
     MatrixXd PHt = P_ * Ht;
     MatrixXd K = PHt * Si;
-
+    
     x_ = x_ + (K * y);
     long x_size = x_.size();
     MatrixXd I = MatrixXd::Identity(x_size, x_size);
@@ -50,9 +50,9 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+    /**
+     * TODO: update the state by using Extended Kalman Filter equations
+     */
     float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
     float phi = atan2(x_(1), x_(0));
     float rho_dot;
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,33 +11,32 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+    /**
+     * TODO: Calculate the RMSE here.
+     */
     VectorXd rmse(4);
     rmse << 0, 0, 0, 0;
     
-    if (estimations.size() != ground_truth.size()
-        || estimations.size() == 0){
+    if (estimations.size() != ground_truth.size() || estimations.size() == 0){
         std::cout << "Invalid data" << std::endl;
         return rmse;
     }
     
-    for (unsigned int i = 0; i < estimations.size(); ++i){
-        VectorXd residual = estimations[i] - ground_truth[i];
-        residual = residual.array()*residual.array();
-        rmse += residual;
+    unsigned int size = estimations.size();
+    for (unsigned int i = 0; i < size; ++i){
+        VectorXd diff = estimations[i] - ground_truth[i];
+        diff = diff.array()*diff.array();
+        rmse += diff;
     }
-    
-    rmse = rmse / estimations.size();
+    rmse = rmse / size;
     return rmse.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+    /**
+     * TODO:
+     * Calculate a Jacobian here.
+     */
     MatrixXd Hj(3, 4);
     float px = x_state(0);
     float py = x_state(1);
@@ -54,8 +53,8 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     }
     
     Hj << (px / c2), (py / c2), 0, 0,
-          -(py / c1), (px / c1), 0, 0,
-          py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+    -(py / c1), (px / c1), 0, 0,
+    py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
     
     return Hj;
 }
